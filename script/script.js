1bff6ecd67be5f82361982dab63a88ba8a2874eb
ckrdi <agung.cokorda@gmail.com>
2021-07-12 21:32:44 +0800

    mobile view and desktop view done

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
-const err = document.getElementById("err");
-const yes = document.getElementById("yes");
+const notification = document.getElementById("notification");
 
 /*
  * validate an e-mail address by leveraging the HTML5
@@ -10,24 +9,23 @@ const yes = document.getElementById("yes");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    err.textContent = "";
-    document.getElementById("err-icon").style.display = "none";
 
     var input = document.createElement("input");
     input.type = "email";
     input.value = email.value;
 
     if (input.checkValidity() && input.value !== "") {
-        yes.textContent = "Thank you for your patronage!";
+        notification.textContent = "Thank you for your patronage!";
         email.value = "";
     } else {
         document.getElementById("err-icon").style.display = "block";
-        err.textContent = "Please provide a valid email.";
+        notification.textContent = "Please provide a valid email.";
     }
 
     return false;
 });
 
 email.addEventListener("focus", function () {
-    yes.textContent = "";
+    notification.textContent = "";
+    document.getElementById("err-icon").style.display = "none";
 });
